d5bf3d2eea04acf7001ed8a15ff81705a659e2b1
BlaineBeltran <bbeltran1998@yahoo.com>
2023-10-05 16:27:02 -0500

    Setup xcode project

---
+++ FriscoTV+/FriscoTV+/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriscoTV+
+//
+//  Created by Blaine Beltran on 10/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
